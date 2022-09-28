52f08bb00356ab11fec9db8da01a7ab3fdb827ea
guoric <eric_guo@brown.edu>
2022-09-27 20:41:06 -0400

    updated website

--- app.js
+++ app.js
@@ -26,15 +26,25 @@ const updatePage = async () => {
 
   // Make API request and get an array of fruit objects
   const fruitsArray = await apiRequest();
-  // console.log(fruitsArray);
+  console.log(fruitsArray);
 
   // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
   // For example, find "name of all fruits whose sugar > 15",
-
+  const filterArray = fruitsArray.filter((fruit) => {
+    return fruit.nutritions.sugar > 15;
+  })
+  const keyArray = filterArray.map((fruit) => {
+    return fruit.name
+  })
   // TODO: Create a new HTML element to display your data
-
+  const newElement = document.createElement('div');
+  newElement.innerHTML = keyArray;
+  // const newArray = new Array();
+  // for (i = 0; i < filterArray.length; i++)
+  //   newElement.append(filterArray[i]);
   // TODO: Append your new element to the page
 
+  gallery.append(newElement);
 }
 
 // SAMPLE CODE of how to create and append a new HTML element to the page
